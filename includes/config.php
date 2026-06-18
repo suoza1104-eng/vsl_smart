88a13ab4dab36b0248ef7a69e24f8075789a1dbd
suoza1104-eng <suoza1104@gmail.com>
2026-06-17 22:43:13 -0300

    Fix default admin password hash

--- includes/config.php
+++ includes/config.php
@@ -13,7 +13,7 @@ define('DB_CHARSET', 'utf8mb4');
 
 define('ADMIN_USER', 'admin');
 // Gere um novo hash em /install/install.php ou usando password_hash('sua-senha', PASSWORD_DEFAULT).
-define('ADMIN_PASS_HASH', '$2y$10$5Yb6LdbYl0ZV.UzV6pgHcOuLcznsx3l4/mEKckCBeRXCmqwkCvg8K'); // admin123
+define('ADMIN_PASS_HASH', '$2y$10$y1JvF415eXBTVUl1ccIre.Rsh0qtMBIA7IQ7I6xJYD54KSDNDur7S'); // admin123
 
 define('SUPERFUNCIONARIO_WEBHOOK_URL', '');
 define('SUPERFUNCIONARIO_TOKEN', '');
@@ -25,4 +25,3 @@ if (DEBUG_MODE) {
     ini_set('display_errors', '0');
     error_reporting(0);
 }
-
